6976ab3e7d9884b01294ade9b9548963f8bdd2a0
Paranyu KITTINAVAKIT <paranyu.lion@gmail.com>
2026-03-08 20:42:48 +0700

    feat,refactor(songs): add get song detail and delete song. refactor old function

--- songs/urls.py
+++ songs/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.song_list, name='song_list'),
     path('create/', views.song_create, name='song_create'),
+    path('<int:song_id>/', views.song_detail, name='song_detail'),
+    path('<int:song_id>/delete/', views.song_delete, name='song_delete'),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,37 +1,86 @@
-from django.http import JsonResponse
+import json
+from django.utils import timezone
+from django.http import JsonResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.models import User
-import json
+
 from .models import Song
 
 
+@require_http_methods(["GET"])
 def song_list(request):
-    if request.method == "GET":
-        songs = Song.objects.all().values()
+    try:
+        songs = Song.objects.filter(deleted_at__isnull=True).values()
         return JsonResponse(list(songs), safe=False, json_dumps_params={'indent': 4})
-    return JsonResponse({"error": "Method not allowed"}, status=405)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
 
 @csrf_exempt
+@require_http_methods(["POST"])
 def song_create(request):
-    if request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            default_user = User.objects.first() 
-            if not default_user:
-                return JsonResponse({"error": "Create user first. Use createsuperuser in termianl"}, status=400)
-            song = Song.objects.create(
-                title=data.get('title'),
-                genre=data.get('genre'),
-                mood=data.get('mood'),
-                ocasion=data.get('ocasion'),
-                prompt=data.get('prompt'),
-                singer_voice=data.get('singer_voice'),
-                creator=default_user,
-                audio_file="demo.mp3",
-                status='Completed'
-            )
-            return JsonResponse({"message": "Created", "id": song.id, "title": song.title}, status=201)
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=400)
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON"}, status=400)
+    
+    default_user = User.objects.first() 
+
+    if not default_user:
+        return JsonResponse({"error": "No user found. Create user first."}, status=400)
+    
+    try:
+        song = Song.objects.create(
+            title=data.get('title'),
+            genre=data.get('genre'),
+            mood=data.get('mood'),
+            ocasion=data.get('ocasion'),
+            prompt=data.get('prompt'),
+            singer_voice=data.get('singer_voice'),
+            creator=default_user,
+            audio_file="demo.mp3",
+            status='Completed'
+        )
+        return JsonResponse({"message": "Created", "id": song.id, "title": song.title}, status=201)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+        
+        
+@require_http_methods(["GET"])
+def song_detail(request, song_id):
+    try:
+        song = get_object_or_404(Song, id=song_id)
+        return JsonResponse({
+            "id": song.id,
+            "title": song.title,
+            "genre": song.genre,
+            "mood": song.mood,
+            "ocasion": song.ocasion,
+            "prompt": song.prompt,
+            "singer_voice": song.singer_voice,
+            "audio_file": song.audio_file.url if song.audio_file else None,
+            "shareable_link": song.shareable_link,
+            "created_at": song.created_at.isoformat(),
+            "status": song.status,
+            })
+    except Http404:
+        return JsonResponse({"error": "Song Not Found"}, status=404)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+
+@csrf_exempt
+@require_http_methods(["DELETE"])
+def song_delete(request, song_id):
+    try:
+        song = get_object_or_404(Song, id=song_id)
+        song.deleted_at = timezone.now()
+        song.save()
+        return JsonResponse({"message": "Deleted (Soft Delete)"}, status=204)
+    except Http404:
+        return JsonResponse({"error": "Not Found"}, status=404)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
